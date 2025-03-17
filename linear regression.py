0ddbe1691cd8766787b698b801547869657953c1
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2025-03-17 17:08:16 +0300

    Update linear regression.py

--- linear regression.py
+++ linear regression.py
@@ -3,3 +3,8 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle 
+
+data=pd.read_csv("student-mat.csv", sep=";")
+
+#print(data.head())
+#print("Columns in dataset:", data.columns.tolist())
