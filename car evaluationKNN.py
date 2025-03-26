11cb707562dae59bb755ef47f4cd19da50e757f6
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2025-03-26 19:07:11 +0300

    Update car evaluationKNN.py

--- car evaluationKNN.py
+++ car evaluationKNN.py
@@ -5,3 +5,5 @@ from sklearn import linear_model
 from sklearn.utils import shuffle
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import linear_model, preprocessing
+
+data=pd.read_csv("car.data")
