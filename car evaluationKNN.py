422d6ad0f4d08a98d94de2bdf580f26c23266408
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2025-03-26 19:03:22 +0300

    Update car evaluationKNN.py

--- car evaluationKNN.py
+++ car evaluationKNN.py
@@ -1,12 +1,1 @@
-Car Evaluation Database was derived from a simple hierarchical decision model originally developed for the demonstration of DEX, M. Bohanec, V. Rajkovic: Expert system for decision making. Sistemica 1(1), pp. 145-157, 1990.). The model evaluates cars according to the following concept structure:
-
-CAR                      car acceptability
-. PRICE                  overall price
-. . buying               buying price
-. . maint                price of the maintenance
-. TECH                   technical characteristics
-. . COMFORT              comfort
-. . . doors              number of doors
-. . . persons            capacity in terms of persons to carry
-. . . lug_boot           the size of luggage boot
-. . safety               estimated safety of the car
+import pandas as pd
