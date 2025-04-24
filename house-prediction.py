9878d0d8c80216daa945a25a5b151224c346c66a
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2025-04-25 01:09:16 +0300

    Update house-prediction.py

--- house-prediction.py
+++ house-prediction.py
@@ -96,5 +96,6 @@ def evaluate_model(y_true, y_pred, model_name):
   rmse = np.sqrt(mean_squared_error(y_true, y_pred))
   r2 = r2_score(y_true, y_pred)
   print(f"{model_name} Performance:")
+  print(f"RMSE: ${rmse:,.2f}")
 
 
